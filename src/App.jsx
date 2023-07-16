8acebea92f8f77794f833251e5f261dde29431c0
Sunkiti <elena.bulavkina@gmail.com>
2023-07-16 19:51:16 +0300

    new project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from './components/Home';
+import Table from './components/Table';
+import Card from './components/Card';
+import './style/App.css';
+
+function App() {
+  return (
+    <>
+      <h1>English cards</h1>
+      <Home></Home>
+      <Table></Table>
+      <Card></Card>
+    </>
+  );
+}
+
+export default App;
